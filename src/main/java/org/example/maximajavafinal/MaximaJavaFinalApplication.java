4cac2265f7f2a61f182ac0a66edbb0325b7a557d
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-02-19 18:22:57 +0700

    Initial commit

---
+++ src/main/java/org/example/maximajavafinal/MaximaJavaFinalApplication.java
@@ -0,0 +1,13 @@
+package org.example.maximajavafinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximaJavaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaximaJavaFinalApplication.class, args);
+	}
+
+}
