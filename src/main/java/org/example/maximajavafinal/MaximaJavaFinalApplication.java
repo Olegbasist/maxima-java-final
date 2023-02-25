900b82f6360020a0ff76ec6462ad701d7a1eb4cd
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-02-25 15:27:06 +0700

    ...

--- src/main/java/org/example/maximajavafinal/MaximaJavaFinalApplication.java
+++ src/main/java/org/example/maximajavafinal/MaximaJavaFinalApplication.java
@@ -1,13 +1,23 @@
 package org.example.maximajavafinal;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MaximaJavaFinalApplication {
+public class MaximaJavaFinalApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaximaJavaFinalApplication.class, args);
 	}
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hallo! I'm Command Runner.");
+		tests();
+	}
+	private static void tests(){
+		System.out.println("Let's start some tests!");
+	}
+
 
 }
