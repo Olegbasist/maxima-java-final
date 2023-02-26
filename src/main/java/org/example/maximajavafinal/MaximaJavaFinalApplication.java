b3c46a603f061d79038e06cf7e4be7bf12a591f4
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-02-26 14:50:20 +0700

    Перенес создание тестовых данных в отдельный класс

--- src/main/java/org/example/maximajavafinal/MaximaJavaFinalApplication.java
+++ src/main/java/org/example/maximajavafinal/MaximaJavaFinalApplication.java
@@ -4,6 +4,7 @@ import org.example.maximajavafinal.model.Excursion;
 import org.example.maximajavafinal.model.Guide;
 import org.example.maximajavafinal.repository.ExcursionRepository;
 import org.example.maximajavafinal.repository.GuideRepository;
+import org.example.maximajavafinal.service.ExcursionService;
 import org.example.maximajavafinal.service.GuideService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,43 +14,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MaximaJavaFinalApplication implements CommandLineRunner {
 
-	@Autowired
-	GuideService guideService;
+	@Autowired TestEntitiesMaker testEntitiesMaker;
 
-	@Autowired
-	ExcursionRepository excursionRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(MaximaJavaFinalApplication.class, args);
 	}
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println("");
+		System.out.println("////////////////////////////////////////////////");
 		System.out.println("Hallo! I'm Command Runner.");
-		addTestEntities();
+		System.out.println("");
+
 		//tests();
-	}
-	private void addTestEntities () {
-		System.out.print("Creating guides ... ");
-		guideService.save(new Guide("Kyle Reese"));
-		guideService.save(new Guide("Peter Silberman"));
-		guideService.save(new Guide("John Connor"));
-		guideService.save(new Guide("Miles Dyson"));
-		guideService.save(new Guide("Sarah Connor"));
-
-		System.out.println("");
-		System.out.println("////////////////////////////////////////////////");
-		System.out.println("Print guides list:");
-		guideService.findAll().forEach(guide -> System.out.println(guide));
-		System.out.println("////////////////////////////////////////////////");
-		System.out.println("");
-
-		System.out.println("Creating excursions and tickets ... ");
-		//excursionRepository.save(new Excursion());
-		Excursion excursion = new Excursion();
-		excursion.setName("First tour");
-		excursion.setDescription("Очень познавательная экскурсия по кракозябрам и буеракам.");
-		excursionRepository.save(excursion);
-		excursionRepository.findAll().forEach(e -> System.out.println(e));
-
 	}
 	private void tests(){
 		System.out.println("Let's do some tests!");
---
+++ src/main/java/org/example/maximajavafinal/TestEntitiesMaker.java
@@ -0,0 +1,72 @@
+package org.example.maximajavafinal;
+
+import jakarta.annotation.PostConstruct;
+import org.example.maximajavafinal.model.Excursion;
+import org.example.maximajavafinal.model.Guide;
+import org.example.maximajavafinal.service.CustomerService;
+import org.example.maximajavafinal.service.ExcursionService;
+import org.example.maximajavafinal.service.GuideService;
+import org.example.maximajavafinal.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TestEntitiesMaker {
+
+    @Autowired
+    GuideService guideService;
+
+    @Autowired
+    ExcursionService excursionService;
+
+    @Autowired
+    TicketService ticketService;
+
+    @Autowired
+    CustomerService customerService;
+
+    public TestEntitiesMaker() {
+    }
+
+    @PostConstruct
+    private void autoMakTestEntities () {
+        makeTestGuides();
+        makeTestExcursions();
+        makeTestExcursions();
+        makeTestExcursions();
+    }
+    public void makeTestGuides () {
+        System.out.println("");
+        System.out.print("Creating guides ... ");
+        guideService.save(new Guide("Kyle Reese"));
+        guideService.save(new Guide("Peter Silberman"));
+        guideService.save(new Guide("John Connor"));
+        guideService.save(new Guide("Miles Dyson"));
+        guideService.save(new Guide("Sarah Connor"));
+
+        System.out.println("");
+        System.out.println("////////////////////////////////////////////////");
+        System.out.println("Print guides list:");
+        guideService.findAll().forEach(guide -> System.out.println(guide));
+        System.out.println("////////////////////////////////////////////////");
+        System.out.println("");
+
+
+    }
+    public void makeTestExcursions () {
+        System.out.println("Creating excursions and tickets ... ");
+        Excursion excursion = new Excursion();
+        excursion.setName("Tour to the new unknown future");
+        excursion.setDescription("Do you know what is the best thing about future? Well, you never feel obliged to take the same route when you are in time loop. At least, that’s what I did when I had a chance to change everything in several years past from now.");
+        excursionService.save(excursion);
+
+        System.out.println("");
+        System.out.println("////////////////////////////////////////////////");
+        System.out.println("");
+        System.out.println("Print excursions list:");
+        excursionService.findAll().forEach(e -> System.out.println(e));
+        System.out.println("");
+        System.out.println("////////////////////////////////////////////////");
+    }
+}
