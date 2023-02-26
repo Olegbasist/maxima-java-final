354a18d24d7fcfe08fdcd3cdaade254ecbc32ab8
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-02-26 12:20:53 +0700

    Create CustomerService.java

---
+++ src/main/java/org/example/maximajavafinal/service/CustomerService.java
@@ -0,0 +1,24 @@
+package org.example.maximajavafinal.service;
+import org.example.maximajavafinal.model.Customer;
+import org.example.maximajavafinal.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerRepository repository;
+
+    public void save (Customer customer) {repository.save(customer);}
+
+    public List<Customer> findAll () {
+        return repository.findAll();
+    }
+
+    public Customer findById (Long id) {return repository.findById(id).get();}
+
+
+    public void deleteById (Long id) {repository.deleteById(id);}
+}
