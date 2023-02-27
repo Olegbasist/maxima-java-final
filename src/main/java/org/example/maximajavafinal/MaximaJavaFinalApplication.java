4b879ac9fdb5b5da37ed167370043d8cd62bb3c8
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-02-27 14:09:56 +0700

    Update MaximaJavaFinalApplication.java

--- src/main/java/org/example/maximajavafinal/MaximaJavaFinalApplication.java
+++ src/main/java/org/example/maximajavafinal/MaximaJavaFinalApplication.java
@@ -42,7 +42,16 @@ public class MaximaJavaFinalApplication implements CommandLineRunner {
 		System.out.println("Hallo! I'm Command Runner.");
 		System.out.println("");
 		//guideService.findAll().forEach(System.out::println);
-		System.out.println(guideService.findByID(1L).getExcursions());
+		System.out.println("");
+		System.out.println("Get excursions connected to guide:");
+		long i = 1;
+		while (i<= guideService.findAll().size()) {
+			System.out.println("");
+			Guide guide = guideService.findByID(i);
+			System.out.println("Guide " + guide.getName() + " signed on:");
+			System.out.println(guide.getExcursions());
+			i++;
+		}
 
 		//List<Excursion> excursions = excursionService.findAll();
 		//excursions.forEach(System.out::println);
