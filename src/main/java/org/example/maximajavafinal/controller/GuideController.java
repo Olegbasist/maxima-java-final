96ea6276f8775f55f7a23d09f696cf2508dd6307
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-08 16:13:43 +0700

    Добавил Гет одного гида

--- src/main/java/org/example/maximajavafinal/controller/GuideController.java
+++ src/main/java/org/example/maximajavafinal/controller/GuideController.java
@@ -1,9 +1,11 @@
 package org.example.maximajavafinal.controller;
 
+import jakarta.persistence.PostUpdate;
 import org.example.maximajavafinal.model.Guide;
 import org.example.maximajavafinal.service.GuideService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -31,5 +33,9 @@ public class GuideController {
     }
     //Ошибка: An error occurred while fetching the resource: AbortError: The operation was aborted.
     //Ушла после аннотирования tickets в Excursion аннотацией @JsonIgnore
+    @GetMapping("/{id}")
+    public Guide getGuid (@PathVariable Long id) {
+        return guideService.findByID(id);
+    }
 
 }
