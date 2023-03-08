5e5fd136f05c3c7446f4b0403ae52749b3e48408
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-08 12:15:19 +0700

    Create ExcursionController.java

---
+++ src/main/java/org/example/maximajavafinal/controller/ExcursionController.java
@@ -0,0 +1,32 @@
+package org.example.maximajavafinal.controller;
+
+import org.example.maximajavafinal.model.Excursion;
+import org.example.maximajavafinal.service.ExcursionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/excursion")
+public class ExcursionController {
+
+    @Autowired
+    private ExcursionService excursionService;
+
+    @GetMapping("/")
+    public String root () {
+        return ("Available requests:"
+                + "{all_excursions}"
+                + "{}"
+                + "{}"
+                + "{}"
+                + "{}");
+    }
+    @GetMapping("all_excursions")
+    public List<Excursion> findAll(){
+        return excursionService.findAll();
+    }
+}
