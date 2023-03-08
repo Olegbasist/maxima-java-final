cdeef9603fc0acf7d598f431e1bf5bf435148635
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-08 16:51:36 +0700

    Сделал пустые NotFound эдвайсы для всех

---
+++ src/main/java/org/example/maximajavafinal/advice/GuideNotFoundAdvice.java
@@ -0,0 +1,15 @@
+package org.example.maximajavafinal.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GuideNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler ()
+    String GuideNotFoundHandler () {
+        return null;
+    }
+}
--- src/main/java/org/example/maximajavafinal/controller/GuideController.java
+++ src/main/java/org/example/maximajavafinal/controller/GuideController.java
@@ -3,6 +3,7 @@ package org.example.maximajavafinal.controller;
 import org.example.maximajavafinal.model.Guide;
 import org.example.maximajavafinal.service.GuideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,8 +34,8 @@ public class GuideController {
     //Ошибка: An error occurred while fetching the resource: AbortError: The operation was aborted.
     //Ушла после аннотирования tickets в Excursion аннотацией @JsonIgnore
     @GetMapping("/{id}")
-    public Guide getGuid (@PathVariable Long id) {
-        return service.findByID(id);
+    public ResponseEntity<Guide> getGuid (@PathVariable Long id) {
+        return ResponseEntity.ok(service.findByID(id));
     }
 
 }
