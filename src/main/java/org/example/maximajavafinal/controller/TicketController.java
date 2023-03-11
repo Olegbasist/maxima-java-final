7b1a265275b979899da0453a49a0b7c31ab24cdd
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-11 16:41:35 +0700

    Добавил схему запросов билету

--- src/main/java/org/example/maximajavafinal/controller/TicketController.java
+++ src/main/java/org/example/maximajavafinal/controller/TicketController.java
@@ -20,11 +20,106 @@ public class TicketController {
     @GetMapping
     public String root () {
         return ("Available requests: "
-                + "GET:{all_tickets}, "
-                + "GET:{id}, "
-                + "POST: {/new}, Content-Type={application/json}, RequestBody={price=price}}, "
-                + "DELETE: {/delete/{id}}, "
-                + "DELETE: {/delete/all}, Content-Type={application/json}, RequestBody={confirm=DELETE}}, "
+                + "[\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"GET\",\n" +
+                "          \"url\": \"http://localhost:8080/ticket\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"\",\n" +
+                "              \"value\": \"\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"GET\",\n" +
+                "          \"url\": \"http://localhost:8080/ticket/all_tickets\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"\",\n" +
+                "              \"value\": \"\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"GET\",\n" +
+                "          \"url\": \"http://localhost:8080/ticket/1\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"\",\n" +
+                "              \"value\": \"\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {}\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"POST\",\n" +
+                "          \"url\": \"http://localhost:8080/ticket/new\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"Content-Type\",\n" +
+                "              \"value\": \"application/json\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"application/x-www-form-urlencoded\",\n" +
+                "            \"params\": [\n" +
+                "              {\n" +
+                "                \"name\": \"price\",\n" +
+                "                \"value\": \"100\"\n" +
+                "              }\n" +
+                "            ],\n" +
+                "            \"text\": \"price=100\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"DELETE\",\n" +
+                "          \"url\": \"http://localhost:8080/ticket/delete/1\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"Content-Type\",\n" +
+                "              \"value\": \"application/json\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"\",\n" +
+                "            \"params\": [],\n" +
+                "            \"text\": \"confirm=DELETE\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"DELETE\",\n" +
+                "          \"url\": \"http://localhost:8080/ticket/delete/all\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"Content-Type\",\n" +
+                "              \"value\": \"application/json\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"application/x-www-form-urlencoded\",\n" +
+                "            \"params\": [\n" +
+                "              {\n" +
+                "                \"name\": \"confirm\",\n" +
+                "                \"value\": \"DELETE\"\n" +
+                "              }\n" +
+                "            ],\n" +
+                "            \"text\": \"confirm=DELETE\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      }\n" +
+                "    ]"
                 );
     }
 
