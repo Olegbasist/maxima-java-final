3e8213c532134c2d69e51cf3eccad9cb2d5ac242
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-11 16:20:46 +0700

    Добавил вывод всех запросов

--- src/main/java/org/example/maximajavafinal/controller/GuideController.java
+++ src/main/java/org/example/maximajavafinal/controller/GuideController.java
@@ -20,12 +20,188 @@ public class GuideController {
     @GetMapping
     public String root () {
         return ("Available requests: "
-                + "GET: {/all_guids}, "
-                + "GET: {/{id}}, "
-                + "GET: {/name/{string}}, "
-                + "POST: {/new}, Content-Type={application/json}, RequestBody={name=name}}, "
-                + "DELETE: {/delete/{id}}, "
-                + "DELETE: {/delete/all}, Content-Type={application/json}, RequestBody={confirm=DELETE}, ONLY AFTER EXCURSIONS WIPED!}, "
+                + "[\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"GET\",\n" +
+                "          \"url\": \"http://localhost:8080/guide\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"\",\n" +
+                "              \"value\": \"\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {}\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"GET\",\n" +
+                "          \"url\": \"http://localhost:8080/guide/all_guids\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"\",\n" +
+                "              \"value\": \"\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {}\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"GET\",\n" +
+                "          \"url\": \"http://localhost:8080/guide/{id}\",\n" +
+                "          \"headers\": [],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"\",\n" +
+                "            \"params\": [],\n" +
+                "            \"text\": \"\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"GET\",\n" +
+                "          \"url\": \"http://localhost:8080/guide/name/{name}\",\n" +
+                "          \"headers\": [],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"\",\n" +
+                "            \"params\": [],\n" +
+                "            \"text\": \"\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"POST\",\n" +
+                "          \"url\": \"http://localhost:8080/guide/new\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"Content-Type\",\n" +
+                "              \"value\": \"application/json\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"application/x-www-form-urlencoded\",\n" +
+                "            \"params\": [\n" +
+                "              {\n" +
+                "                \"name\": \"name\",\n" +
+                "                \"value\": \"{name}\"\n" +
+                "              }\n" +
+                "            ],\n" +
+                "            \"text\": \"name={name}\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"PATCH\",\n" +
+                "          \"url\": \"http://localhost:8080/guide/update\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"Content-Type\",\n" +
+                "              \"value\": \"application/json\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"application/x-www-form-urlencoded\",\n" +
+                "            \"params\": [\n" +
+                "              {\n" +
+                "                \"name\": \"id\",\n" +
+                "                \"value\": \"{id}\"\n" +
+                "              },\n" +
+                "              {\n" +
+                "                \"name\": \"name\",\n" +
+                "                \"value\": \"{name}\"\n" +
+                "              }\n" +
+                "            ],\n" +
+                "            \"text\": \"id={id}&name={name}\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"POST\",\n" +
+                "          \"url\": \"http://localhost:8080/guide/service/assign\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"Content-Type\",\n" +
+                "              \"value\": \"application/json\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"application/x-www-form-urlencoded\",\n" +
+                "            \"params\": [\n" +
+                "              {\n" +
+                "                \"name\": \"guide_id\",\n" +
+                "                \"value\": \"{id}\"\n" +
+                "              },\n" +
+                "              {\n" +
+                "                \"name\": \"excursion_id\",\n" +
+                "                \"value\": \"{id}\"\n" +
+                "              }\n" +
+                "            ],\n" +
+                "            \"text\": \"guide_id={id}&excursion_id={id}\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"POST\",\n" +
+                "          \"url\": \"http://localhost:8080/guide/service/relive\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"Content-Type\",\n" +
+                "              \"value\": \"application/json\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"application/x-www-form-urlencoded\",\n" +
+                "            \"params\": [\n" +
+                "              {\n" +
+                "                \"name\": \"id\",\n" +
+                "                \"value\": \"{id}\"\n" +
+                "              }\n" +
+                "            ],\n" +
+                "            \"text\": \"id={id}\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"DELETE\",\n" +
+                "          \"url\": \"http://localhost:8080/guide/delete/{id}\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"Content-Type\",\n" +
+                "              \"value\": \"application/json\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"\",\n" +
+                "            \"params\": [],\n" +
+                "            \"text\": \"confirm=DELETE\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"request\": {\n" +
+                "          \"method\": \"DELETE\",\n" +
+                "          \"url\": \"http://localhost:8080/guide/delete/all\",\n" +
+                "          \"headers\": [\n" +
+                "            {\n" +
+                "              \"name\": \"Content-Type\",\n" +
+                "              \"value\": \"application/json\"\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"postData\": {\n" +
+                "            \"mimeType\": \"\",\n" +
+                "            \"params\": [],\n" +
+                "            \"text\": \"confirm=DELETE\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      }\n" +
+                "    ]"
                 );
     }
     
