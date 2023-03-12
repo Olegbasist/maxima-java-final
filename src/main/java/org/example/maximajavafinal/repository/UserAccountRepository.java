6325f43aba638e1123f8c734df4dcf2cffd18926
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-12 11:28:52 +0700

    Сделал свой username

--- src/main/java/org/example/maximajavafinal/repository/UserAccountRepository.java
+++ src/main/java/org/example/maximajavafinal/repository/UserAccountRepository.java
@@ -2,8 +2,11 @@ package org.example.maximajavafinal.repository;
 
 import org.example.maximajavafinal.model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserAccountRepository extends JpaRepository <UserAccount, Long> {
+
+    UserDetails findUserAccountByUsername (String username);
 }
---
+++ src/main/java/org/example/maximajavafinal/service/JpaUserDetailsService.java
@@ -0,0 +1,21 @@
+package org.example.maximajavafinal.service;
+
+import org.example.maximajavafinal.model.UserAccount;
+import org.example.maximajavafinal.repository.UserAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserAccountRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findUserAccountByUsername(username);
+    }
+}
