2d88e7a407c6e0619f1b678b82dd450cdd286193
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-12 14:15:28 +0700

    Create SecurityConfig.java

---
+++ src/main/java/org/example/maximajavafinal/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package org.example.maximajavafinal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+}
