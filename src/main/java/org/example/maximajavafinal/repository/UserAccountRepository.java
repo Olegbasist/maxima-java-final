0d0c3b7ba4ca8f9837992b98e615cb09c42a4653
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-12 11:15:29 +0700

    Create UserAccountRepository.java

---
+++ src/main/java/org/example/maximajavafinal/repository/UserAccountRepository.java
@@ -0,0 +1,9 @@
+package org.example.maximajavafinal.repository;
+
+import org.example.maximajavafinal.model.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository <UserAccount, Long> {
+}
