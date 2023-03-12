2d91a9aae8aaed05266d028830c073241c5664f3
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-12 17:29:26 +0700

    ...

---
+++ src/main/java/org/example/maximajavafinal/service/RSAKeys.java
@@ -0,0 +1,20 @@
+package org.example.maximajavafinal.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Service
+public class RSAKeys {
+
+    private RSAPrivateKey privateKey;
+    private RSAPublicKey publicKey;
+
+    @Value("${rsa.privatekey.path}")
+    private String privetKeyPath;
+
+    @Value("${rsa.publickey.path}")
+    private String publicKeyPath;
+}
