b3a367cf051dd5dcf717d13ac0b43f77bceecb71
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-12 10:52:44 +0700

    Сделал базовый UserAccount

---
+++ src/main/java/org/example/maximajavafinal/model/UserAccount.java
@@ -0,0 +1,71 @@
+package org.example.maximajavafinal.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class UserAccount implements UserDetails {
+
+    @Id @GeneratedValue
+    private long id;
+
+    private String username;
+
+    private String password;
+
+    private List<String> authorities;
+
+    private boolean enabled;
+
+    public UserAccount() {
+    }
+
+    public UserAccount(String username, String password, List<String> authorities, boolean enabled) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities.stream().map(SimpleGrantedAuthority::new).toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
