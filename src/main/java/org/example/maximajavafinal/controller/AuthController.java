6e33492a6cc7e664123b7a2221f19cf1a15dc156
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-03-12 13:58:46 +0700

    Пустой контроллер

--- src/main/java/org/example/maximajavafinal/controller/AuthController.java
+++ src/main/java/org/example/maximajavafinal/controller/AuthController.java
@@ -1,4 +1,15 @@
 package org.example.maximajavafinal.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
 public class AuthController {
+
+    @GetMapping ("/hallo")
+    public String auth (Principal principal) {
+        return ("Hallo " + principal.getName() +"!");
+    }
 }
